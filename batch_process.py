8a8818b7cf9ed2ffc3e747798093206f4e77d7a9
CrashedBboy <crashedbboy@gmail.com>
2019-12-04 14:19:06 +0800

    script of batch processing

---
+++ batch_process.py
@@ -0,0 +1,31 @@
+from referencer import main
+
+import os
+
+source_directory = "./data/source"
+target_directory = "./data/target"
+
+start_level = 0
+end_level = 3
+
+if not os.path.isdir(target_directory):
+    os.makedirs(target_directory)
+
+for level in range(start_level, end_level+1):
+    
+    target_level_dir = target_directory + "/" + str(level)
+    source_level_dir = source_directory + "/" + str(level)
+
+    if not os.path.isdir(target_level_dir):
+        os.makedirs(target_level_dir)
+
+    max = 2**level
+
+    for x in range(0, max):
+        for y in range(0, max):
+
+            source_file = source_level_dir + "/" + str(x) + "_" + str(y) + ".jpg"
+
+            if (os.path.isfile(source_file)):
+                print("process " + str(x) + ", " + str(y) + ", " + str(level))
+                main.georeference (x, y, level, source_file, target_level_dir + "/" + str(x) + "_" + str(y) + ".tiff")
